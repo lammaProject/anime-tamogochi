27590949e71a64fc1cb061f1722611ed9516e825
Anton <118883994+lammaProject@users.noreply.github.com>
2026-03-01 18:49:52 +0500

    feat: add TestPage component

---
+++ frontend/src/pages/TestPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test-page">
+    <p>test</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.test-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
